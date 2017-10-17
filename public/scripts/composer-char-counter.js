1015fd0fa8697e8cd8392380a2931e04a95dbef6
broodmeister <isaiahchrst80@gmail.com>
2017-10-17 00:11:57 +0000

    Implemented a character counter for Tweets.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".new-tweet form textarea").on("input", function(event) {
+    const limit = 140;
+    var length = $(this).val().length;
+    var remain = limit - length;
+    var counter = $(this).parent().find(".counter");
+    counter.text(remain);
+    if (remain < 0) {
+      counter.addClass("err");
+    } else {
+      counter.removeClass("err");
+    }
+  });
+});
