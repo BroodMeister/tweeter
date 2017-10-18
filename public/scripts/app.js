b2e0a1b227f7d0de79d8a8f31627c0f7dbd6134e
broodmeister <isaiahchrst80@gmail.com>
2017-10-18 18:54:22 +0000

    Removed hard-coded tweets, added event to prevent default behavior, and changed app to do form submissions through jQuery.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,11 +29,7 @@ $(document).ready(function() {
   function createTweetElement(tweet) {
      // header section
     let $header = $("<header>");
-    let $avatar = $("<img>",
-      {
-        class: "avatar",
-        src: tweet.user.avatars.small
-      });
+    let $avatar = $("<img>", { class: "avatar", src: tweet.user.avatars.small });
     let $name = $("<h2>", { class: "name" }).text(tweet.user.name);
     let $handle = $("<p>", { class: "handle" }).text(tweet.user.handle);
     $header.append($avatar);
@@ -66,53 +62,14 @@ $(document).ready(function() {
     });
   }
 
-  // Fake data taken from tweets.json
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
-  renderTweets(data);
+  $("#submit").on("click", function(event) {
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $(this).siblings('textarea').serialize(),
+      success: function() {
+      }
+    });
+    event.preventDefault();
+  });
 });
